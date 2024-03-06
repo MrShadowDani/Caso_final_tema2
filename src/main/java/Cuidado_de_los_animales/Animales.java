17645dee88b92ab42ca2ec33623982c5a9174a2b
MrShadowDani <dgonzsan@myuax.com>
2024-03-06 13:22:39 +0100

    Avances

--- src/main/java/Cuidado_de_los_animales/Animales.java
+++ src/main/java/Cuidado_de_los_animales/Animales.java
@@ -22,6 +22,26 @@ public class Animales {
     private int id;
 
     //Constructor
+    public Animales(String nombre, String especie, String raza, String color, String sexo, String edad, String peso, String altura, String tipo, String habitat, String alimentacion, String reproduccion, String cuidados, String enfermedades, String vacunas, boolean salud, boolean comportamiento, int id) {
+        this.nombre = nombre;
+        this.especie = especie;
+        this.raza = raza;
+        this.color = color;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.peso = peso;
+        this.altura = altura;
+        this.tipo = tipo;
+        this.habitat = habitat;
+        this.alimentacion = alimentacion;
+        this.reproduccion = reproduccion;
+        this.cuidados = cuidados;
+        this.enfermedades = enfermedades;
+        this.vacunas = vacunas;
+        this.salud = salud;
+        this.comportamiento = comportamiento;
+        this.id = id;
+    }
 
 
 
