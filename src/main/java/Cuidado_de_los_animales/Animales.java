f934218d83d6132fab35e464bdae15886c4bebd7
MrShadowDani <dgonzsan@myuax.com>
2024-03-06 11:54:23 +0100

    Avances

---
+++ src/main/java/Cuidado_de_los_animales/Animales.java
@@ -0,0 +1,29 @@
+package Cuidado_de_los_animales;
+
+public class Animales {
+    //Caracteristicas de los animales
+    private String nombre;
+    private String especie;
+    private String raza;
+    private String color;
+    private String sexo;
+    private String edad;
+    private String peso;
+    private String altura;
+    private String tipo;
+    private String habitat;
+    private String alimentacion;
+    private String reproduccion;
+    private String cuidados;
+    private String enfermedades;
+    private String vacunas;
+    private boolean salud;
+    private boolean comportamiento;
+    private int id;
+
+    //Constructor
+
+
+
+
+}
